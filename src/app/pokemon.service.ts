4e1655633599d3a92318ea5e0e6435d35a1b82bd
Camilo García <gcgarciab@unal.edu.co>
2021-07-23 19:50:35 -0500

    Obtener lista desde el api

--- src/app/pokemon.service.ts
+++ src/app/pokemon.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +11,10 @@ export class PokemonService {
 
   constructor(private http: HttpClient) { }
 
+  getPokemons(): Observable<any> {
+    return this.http.get('https://pokeapi.co/api/v2/pokemon/', {
+      headers: this.headers
+    });
+  }
   
 }
